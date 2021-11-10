d9b081ba568b7baead30e6ccdca169e3fe2014d8
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-11-09 20:45:23 -0500

    Create header.js

---
+++ header.js
@@ -0,0 +1,11 @@
+function home() {
+  window.location.href = "./index.html"
+}
+
+function games() {
+  window.location.href = "./games.html"
+}
+
+function about() {
+  window.location.href = "./about.html"
+}
