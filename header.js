978be87d16a94a75e6e621c40d482b62c0e18b9e
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-11-09 20:59:10 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 function home() {
-  window.top.location.href = "./index.html"
+  top.location.href = "./index.html"
 }
 
 function games() {
-  window.top.location.href = "./games.html"
+  top.location.href = "./games.html"
 }
 
 function about() {
-  window.top.location.href = "./about.html"
+  top.location.href = "./about.html"
 }
